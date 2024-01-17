7d4e2e013f1be73f89b5c8824aa46e8499a23c63
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-17 04:20:26 +0200

    Update setup_mysql_dev.sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- Create or use the database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create or use the user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
