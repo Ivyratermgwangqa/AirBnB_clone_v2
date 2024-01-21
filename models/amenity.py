056061fd96bf49a5d1ce702880989ef47a2c4c06
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-21 05:15:16 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""This is the Amenity class"""
+"""This is the class for Amenity"""
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-
+from os import getenv
 
 class Amenity(BaseModel, Base):
     """This is the class for Amenity
-
     Attributes:
-        name (str): Amenity name
-        place_amenities (relationship): Relationship with Place instances
+        name: name of the Amenity
     """
     __tablename__ = 'amenities'
-    
-    name = Column(String(128), nullable=False)
-
-    # Specify lazy loading for the relationship
-    place_amenities = relationship('Place',
-                                   secondary='place_amenity',
-                                   backref='amenities',
-                                   lazy='select',
-                                   cascade='delete')
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship('PlaceAmenity', back_populates='amenity')
+        place_amenities = relationship('PlaceAmenity', back_populates='amenity')
+    else:
         name = ""
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def place_amenities(self):
+            """Getter method to return the list of PlaceAmenity instances"""
+            from models import storage
+            place_amenities_list = storage.all('PlaceAmenity').values()
+            return [place_amenity for place_amenity in place_amenities_list if place_amenity.amenity_id == self.id]
