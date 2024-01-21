17877c75b6829065e9cdf2955eeafa854e01fb0d
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-21 04:00:28 +0200

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,42 +1,52 @@
 #!/usr/bin/python3
-""" Place instance with a city_id, amenities and reviews """
-from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
+"""This is the Place class."""
 from models.base_model import BaseModel, Base
-from os import getenv
-from models.review import Review
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table, MetaData
 from sqlalchemy.orm import relationship
 
-STORAGE = getenv("HBNB_TYPE_STORAGE")
-
+metadata = Base.metadata
 
 class Place(BaseModel, Base):
-    """ A place to stay """
+    """This is the class for Place
+
+    Attributes:
+        city_id (str): City id (foreign key)
+        user_id (str): User id (foreign key)
+        name (str): Place name
+        description (str): Description of the place
+        number_rooms (int): Number of rooms
+        number_bathrooms (int): Number of bathrooms
+        max_guest (int): Maximum number of guests
+        price_by_night (int): Price for a night's stay
+        latitude (float): Latitude
+        longitude (float): Longitude
+        amenity_ids (relationship): Relationship with Amenity instances
+    """
+
     __tablename__ = 'places'
-    if STORAGE == "db":
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True)
-        longitude = Column(Float, nullable=True)
-        reviews = relationship('Review',
-                               backref='place',
-                               cascade="all, delete")
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
 
-        place_amenity = Table('place_amenity', Base.metadata,
-                              Column('place_id', String(60),
-                                     ForeignKey('places.id'),
-                                     primary_key=True, nullable=False),
-                              Column('amenity_id', String(60),
-                                     ForeignKey('amenities.id'),
-                                     primary_key=True, nullable=False))
+    # Specify lazy loading for the relationship
+    amenity_ids = relationship('Amenity', secondary='place_amenity', viewonly=False, lazy='select')
 
-        amenities = relationship('Amenity', secondary=place_amenity,
-                                 viewonly=False, backref="places")
+    place_amenity = Table('place_amenity', metadata,
+                          Column('place_id',
+                                 String(60),
+                                 ForeignKey('places.id'),
+                                 primary_key=True, nullable=False),
+                          Column('amenity_id',
+                                 String(60),
+                                 ForeignKey('amenities.id'),
+                                 primary_key=True, nullable=False))
 
     else:
         city_id = ""
