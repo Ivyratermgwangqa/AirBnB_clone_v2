03610a73a6f27c37637269dcd3942bce2536e5c2
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-01-21 05:11:56 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
@@ -8,24 +9,22 @@ class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
-        password: password for you login
+        password: password for your login
         first_name: first name
         last_name: last name
     """
     __tablename__ = 'users'
-    if STORAGE == "db":
-      email = Column(String(128), nullable=False)
-      password = Column(String(128), nullable=False)
-      first_name = Column(String(128),nullable=True)
-      last_name = Column(String(128), nullable=True)
-      reviews = relationship('Review', backref='user',
-                           cascade='delete')
-      places = relationship('Place', backref='user',
-                          cascade='delete')
-      base_model_id = Column(String(60), ForeignKey('base_model.id'))
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        reviews = relationship('Review', backref='user', cascade='delete')
+        places = relationship('Place', backref='user', cascade='delete')
+        base_model_id = Column(String(60), ForeignKey('base_model.id'))
 
     else:
-      email = ""
-      password = ""
-      first_name = ""
-      last_name = ""
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
