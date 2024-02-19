d208dad5c6995cbcb7c32414eb0fce26876db1bc
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-19 02:15:52 +0200

    Update 10-hbnb_filters.py

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
+"""Starts a Flask web application"""
 
-from models import storage
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
 
-app = Flask('web_flask')
-
-
-@app.route('/hbnb_filters', strict_slashes=False)
-def display_filters():
-    states = storage.all('State')
-    amenities = storage.all('Amenity')
-    return render_template('10-hbnb_filters.html',
-                           states=states, amenities=amenities)
+app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown(exc):
+def teardown_db(exception):
+    """Closes the database"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Displays HBNB filters"""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    amenities = storage.all(Amenity).values()
+    sorted_amenities = sorted(amenities, key=lambda amenity: amenity.name)
+    return render_template('10-hbnb_filters.html', states=sorted_states, amenities=sorted_amenities)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
