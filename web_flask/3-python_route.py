20dae6ed5efb104bd7004d20bd7020fd56f2485a
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-19 02:08:31 +0200

    Update 3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -21,14 +21,14 @@ def display_hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def display_c(text):
     """Displays 'C ' followed by the value of the text variable"""
-    return 'C {}'.format(text.replace('_', ' '))
+    return 'C {}'.format(text.replace('_', " "))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text):
     """Displays 'Python ' followed by the value of the text variable"""
-    return 'Python {}'.format(text.replace('_', ' '))
+    return 'Python {}'.format(text.replace('_', " "))
 
 
 if __name__ == '__main__':
