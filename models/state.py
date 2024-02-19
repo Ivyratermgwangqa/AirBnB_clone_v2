f166b7f0c9bb8cc58fa53d78b410a4ee31b95809
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-19 03:13:21 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+"""This is the state class."""
+from models.base_model import Base, BaseModel
 from models.city import City
-from os import getenv
-
-STORAGE = getenv("HBNB_TYPE_STORAGE")
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
+import os
 
 
 class State(BaseModel, Base):
-    """ State class for the states of a country """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = 'states'
-    if STORAGE == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship(
-            'City', backref='state', cascade="all, delete-orphan")
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='delete')
 
+    if ("HBNB_TYPE_STORAGE", None) is None:
         @property
         def cities(self):
-            from models import storage
-            list_city = []
-            all_ins = storage.all(City)
-            for value in all_ins.values():
-                if value.state_id == self.id:
-                    list_city.append(value)
-            return list_city
+            Clist = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return Clist
