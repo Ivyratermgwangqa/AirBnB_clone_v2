fdce43e8d436c287bd8422042d1f32f338cee4da
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-19 02:07:05 +0200

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -9,26 +9,26 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Displays 'Hello HBNB!'"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def display_hbnb():
     """Displays 'HBNB'"""
-    return 'HBNB'
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def display_c(text):
     """Displays 'C ' followed by the value of the text variable"""
-    return 'C {}'.format(text.replace('_', ' '))
+    return 'C {}'.format(text.replace('_', " "))
 
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def display_python(text):
     """Displays 'Python ' followed by the value of the text variable"""
-    return 'Python {}'.format(text.replace('_', ' '))
+    return 'Python {}'.format(text.replace('_', " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
